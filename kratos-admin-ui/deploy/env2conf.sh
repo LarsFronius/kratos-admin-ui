85e145cf60c82f177837e335280ea25eeaa492bb
Lars Fronius <lars@fronius.me>
2025-09-28 16:50:27 +0200

    make reverse proxying configurable

--- kratos-admin-ui/deploy/env2conf.sh
+++ kratos-admin-ui/deploy/env2conf.sh
@@ -2,9 +2,10 @@
 ##
 # Usage: env2conf dir filename
 # env2conf.sh public/config.json
-##
+
+REVERSE_PROXY_SETTINGS="${REVERSE_PROXY:=false}"
 
 mkdir -p "$(dirname "$1")" && touch "$1"
 echo '{' >"$1"
-echo '   "reverseProxy": true' >> "$1"
+echo "   \"reverseProxy\": ${REVERSE_PROXY_SETTINGS}" >> "$1"
 echo '}' >> "$1"
--- kratos-admin-ui/deploy/start.sh
+++ kratos-admin-ui/deploy/start.sh
@@ -1,13 +1,3 @@
-if [ -z ${NAMESERVER+x} ]; then
-    if [ -f /etc/resolv.conf ]; then
-        export NAMESERVER=$(awk '/^nameserver/{print $2}' /etc/resolv.conf)
-    else
-        # see https://www.emmanuelgautier.com/blog/nginx-docker-dns-resolution
-        export NAMESERVER="127.0.0.11"
-    fi
-fi
-
-echo "Nameserver is: $NAMESERVER"
 
 checkFormat() {
     regex="http?(s)://"
